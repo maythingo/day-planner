375797248a9e1eee3532d2bc1a015c95ec8ef940
maythingo <maythingo@aim.com>
2021-04-17 19:45:53 -0700

    working on local storage, made sure targeting right sibling by using closest, still cannot get value to stay after being refreshed

--- script.js
+++ script.js
@@ -27,37 +27,29 @@ function updatetime(){
 
 var save = document.querySelector(".saveBtn");
 
+
 save.addEventListener("click", function(){
-    var time = $(this).parent().attr("id");
+    var time = $(this).closest("td").attr("id");
     var textContent = $("textarea").val().trim();
     localStorage.setItem(time, textContent);
     console.log(time, textContent);
 });
 
 
-      /*
-$(".saveBtn").on("click", function() {
-    var time = $(this).parent().attr("class");
-    var textContent = $("textarea").val().trim();
-    
-    localStorage.setItem(time, textContent);
-    console.log(time, textContent);
-}); */
-/*
-$("#9AM").children("textarea").val(localStorage.getItem("9AM"));
+$("#9AM").next("textarea").val(localStorage.getItem("9AM"));
 
-$("#10AM").children("textarea").val(localStorage.getItem("10AM"));
+$("#10AM").next("textarea").val(localStorage.getItem("10AM"));
 
-$("#11AM").children("textarea").val(localStorage.getItem("11AM"));
+$("#11AM").next("textarea").val(localStorage.getItem("11AM"));
 
-$("#12PM").children("textarea").val(localStorage.getItem("12PM"));
+$("#12PM").next("textarea").val(localStorage.getItem("12PM"));
 
-$("#1PM").children("textarea").val(localStorage.getItem("1PM"));
+$("#1PM").next("textarea").val(localStorage.getItem("1PM"));
 
-$("#2PM").children("textarea").val(localStorage.getItem("2PM"));
+$("#2PM").next("textarea").val(localStorage.getItem("2PM"));
 
-$("#3PM").children("textarea").val(localStorage.getItem("3PM"));
+$("#3PM").next("textarea").val(localStorage.getItem("3PM"));
 
-$("#4PM").children("textarea").val(localStorage.getItem("4PM"));
+$("#4PM").next("textarea").val(localStorage.getItem("4PM"));
 
-$("#5PM").children("textarea").val(localStorage.getItem("5PM"));*/
+$("#5PM").next("textarea").val(localStorage.getItem("5PM"));
